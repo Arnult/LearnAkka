1509d261cebb092b589492bbc805a62e5410a781
lhc <gzeng1729@gmail.com>
2019-11-09 23:37:23 +0800

    test: 测试StringActor 接收String Message的情况

--- src/test/scala/com/ch1/StringActorSpec.scala
+++ src/test/scala/com/ch1/StringActorSpec.scala
@@ -1,5 +1,21 @@
 package com.ch1
 
-class StringActorSpec {
+import akka.actor.ActorSystem
+import akka.testkit.TestActorRef
+import org.scalatest.{FunSpecLike, Matchers}
 
+class StringActorSpec extends FunSpecLike with Matchers{
+    implicit val system = ActorSystem()
+  describe("StringActor"){
+    describe("get String message"){
+      it("should store string message"){
+        val actorRef = TestActorRef(new StringActor)
+        actorRef ! SetStringRequset("hello")
+        val stringActor = actorRef.underlyingActor
+        stringActor.message.toString should equal("hello")
+        actorRef ! "world"
+        stringActor.message.toString should equal("world")
+      }
+    }
+  }
 }
