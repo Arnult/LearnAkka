6cfcf65874dca88b918e1ff8ffae092697e6c9b2
lhc <gzeng1729@gmail.com>
2019-11-09 23:36:07 +0800

    feat: 接收String Message

--- src/main/scala/com/ch1/StringActor.scala
+++ src/main/scala/com/ch1/StringActor.scala
@@ -1,5 +1,21 @@
 package com.ch1
 
-class StringActor {
+import akka.actor.Actor
+import akka.event.Logging
 
+class StringActor extends Actor{
+  val message = new StringBuffer()
+  val log = Logging(context.system,this)
+  override def receive: Receive = {
+    case SetStringRequset(value) =>{
+      log.info("Received SetStringRequest :"+value)
+      message.delete(0,message.length())
+      message.append(value)
+    }
+    case "world" =>{
+      log.info("Received String")
+      message.delete(0,message.length())
+      message.append("world")
+    }
+  }
 }
